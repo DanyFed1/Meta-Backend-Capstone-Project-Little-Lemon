d2593f87f4da25fff649c057a31823d92ccf42c5
DanyFed1 <danyfed.study@gmail.com>
2023-11-01 21:44:01 +0100

    Added menu and booking models

--- LittleLemonProject/settings.py
+++ LittleLemonProject/settings.py
@@ -75,12 +75,20 @@ WSGI_APPLICATION = "LittleLemonProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#Settings.py 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
     }
-}
 
 
 # Password validation
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=225)
+    no_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+    
+    class Meta:
+        db_table = 'booking'
+    
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+    
+    class Meta:
+        db_table = 'menu'
+    
+    def __str__(self):
+        return self.title
